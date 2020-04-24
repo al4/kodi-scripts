e63b0f0f0713abfefbf8cf5d203ab9fd234a0752
Alex Forbes <al4@users.noreply.github.com>
2020-04-24 23:08:23 +0100

    First try at Skype Launcher

---
+++ README.md
@@ -0,0 +1,9 @@
+Skype Launcher for Kodi
+=======================
+
+This is an extremely simple script for Kodi that simply launches Skype on Linux only. It literally just runs the application binary at /usr/bin/skypeforlinux.
+
+Installation
+------------
+This plugin is not in any repository, it must be installed by the "Install from zip file" option. Download the archive from github releases.
+
--- addon.py
+++ addon.py
@@ -1,11 +1,29 @@
+import subprocess
+import sys
 import xbmcaddon
 import xbmcgui
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
 
-# Set a string variable to use 
-line1 = "Hello World! We can write anything we want here Using Python"
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
 
-# Launch a dialog box in kodi showing the string variable 'line1' as the contents
-xbmcgui.Dialog().ok(addonname, line1)
+
+def log(text, level=xbmc.LOGINFO):
+    msg = "SkypeLauncher: " + text
+    xbmc.log(msg, level=level)
+
+
+def kodi_notify(text, level=xbmcgui.NOTIFICATION_INFO, title='Skype Launcher', time=5000):
+    xbmcgui.Dialog().notification(title, text, level, time)
+
+    xbmc.log(log_text.encode('utf-8'), level=xbmc.LOGERROR)
+
+
+def run_skype():
+    if sys.platform.startswith("linux"):
+        subprocess.Popen(['/usr/bin/skypeforlinux'], close_fds=True)
+    else:
+        log("Can't launch Skype on this computer")
+        kodi_notify(xbmcgui.NOTIFICATION_ERROR, "Sorry, only linux is supported")
+
+
+run_skype()
